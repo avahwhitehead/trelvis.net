582f6fdb53c257365644f47c2ddfdcec52da1934
Avah Whitehead <avah.r.whitehead@gmail.com>
2023-12-31 22:42:03 +0000

    Added basic template

--- build.ps1
+++ build.ps1
@@ -1,1 +1,3 @@
-asciidoctor -b html5 -D docs/ -R . *.adoc
+asciidoctor -a stylesheet=styles.css -b html5 -D docs/ -R . *.adoc
+
+#asciidoctor-serve -a stylesheet=styles.css -b html5 -D docs/ -R . *.adoc --ignore notes.adoc
